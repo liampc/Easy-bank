f35073e978d1a7eb2db9ced27ad468d0989ac0db
liampc <kathliampcarlos@gmail.com>
2021-01-17 12:05:25 +0800

    transform menu icon

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+const burgerIcon = document.querySelector('#burger-icon')
+
+burgerIcon.addEventListener('click', () => {
+    console.log('burger')
+
+    if (burgerIcon.classList.contains('open')){
+        burgerIcon.classList.remove('open')
+    } else {
+        burgerIcon.classList.add('open')
+    }
+
+    
+})
