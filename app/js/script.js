470528bab80b0ea5c072caebe883a23d1b526541
liampc <kathliampcarlos@gmail.com>
2021-01-18 14:29:42 +0800

    add noscroll to body in toggle

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body')
 const burgerIcon = document.querySelector('#burger-icon')
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
@@ -7,21 +8,22 @@ burgerIcon.addEventListener('click', () => {
     console.log('burger')
 
     if (header.classList.contains('open')){
+        body.classList.remove('noscroll')
         header.classList.remove('open')
         fadedElems.forEach(elem => {
             elem.classList.remove('fade-in')
             elem.classList.add('fade-out')
         })
         
-        
-        
 
     } else {
+        body.classList.add('noscroll')
         header.classList.add('open')
         fadedElems.forEach(elem => {
             elem.classList.remove('fade-out')
             elem.classList.add('fade-in')
         })
+        
     }
 
     
