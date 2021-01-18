e32464abc83387cadba1dc81115b9b1e9f5a40f9
liampc <kathliampcarlos@gmail.com>
2021-01-18 11:28:04 +0800

    fade in out overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,21 @@
 const burgerIcon = document.querySelector('#burger-icon')
 const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
 
 burgerIcon.addEventListener('click', () => {
     console.log('burger')
 
     if (header.classList.contains('open')){
+        overlay.classList.remove('fade-in')
+        overlay.classList.add('fade-out')
         header.classList.remove('open')
+        
+        
+
     } else {
         header.classList.add('open')
+        overlay.classList.remove('fade-out')
+        overlay.classList.add('fade-in')
     }
 
     
